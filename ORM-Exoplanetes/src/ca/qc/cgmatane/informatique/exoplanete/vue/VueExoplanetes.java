19f6d9f74d9b0b305a116dd5b9e5ecaa55f65000
DouvryValentin <valentin.jabba@gmail.com>
2018-03-08 18:31:58 -0500

    ajout Text

--- ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/vue/VueExoplanetes.java
+++ ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/vue/VueExoplanetes.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class VueExoplanetes extends Application
 {
 	protected StackPane racine;
  	protected String stringTest;
-	
+	protected Text texteExoplanetes;
 	
 	public void start(Stage scenePrincipale)
 	{
 		stringTest = "Hello word";
 		
 		this.racine = new StackPane();
+		
 		
 	}
 }
