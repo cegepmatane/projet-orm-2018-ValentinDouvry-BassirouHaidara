4d8a5e71a92a62c90bab7a8f9f810ccec1940231
DouvryValentin <valentin.jabba@gmail.com>
2018-03-08 17:53:52 -0500

    Ajout test Vue

--- ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/vue/VueExoplanetes.java
+++ ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/vue/VueExoplanetes.java
@@ -1,5 +1,31 @@
 package ca.qc.cgmatane.informatique.exoplanete.vue;
 
-public class VueExoplanetes {
+import java.util.ArrayList;
+import java.util.List;
 
+import javafx.application.Application;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class VueExoplanetes extends Application
+{
+	protected StackPane racine;
+ 	protected List<String> listeTest;
+	
+	
+	public void start(Stage scenePrincipale)
+	{
+		this.listeTest = new ArrayList<String>();
+		listeTest.add("OUI");
+		listeTest.add("NON");
+		listeTest.add("Bonjour");
+		listeTest.add("Coucou");
+		listeTest.add("Salut");
+		listeTest.add("OUI");
+		listeTest.add("Yo");
+		listeTest.add("Hello");
+		
+		this.racine = new StackPane();
+		
+	}
 }
