d5f04b716ebf371cb52f5768e34a405cf6ea9a15
DouvryValentin <valentin.jabba@gmail.com>
2018-03-09 01:12:12 -0500

    Ajout Setter + DAO

--- ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/action/ControleurExoplanetes.java
+++ ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/action/ControleurExoplanetes.java
@@ -1,14 +1,28 @@
 package ca.qc.cgmatane.informatique.exoplanete.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.qc.cgmatane.informatique.exoplanete.dao.DaoExoplanetes;
+import ca.qc.cgmatane.informatique.exoplanete.modele.Exoplanete;
 import ca.qc.cgmatane.informatique.exoplanete.vue.VueExoplanetes;
 
 public class ControleurExoplanetes {
 	
 	protected VueExoplanetes vueExoplanetes;
+	protected DaoExoplanetes daoExoplanetes;
+	protected List<Exoplanete> listeExoplanete;
 	
 	public ControleurExoplanetes(VueExoplanetes vue)
 	{
+		this.listeExoplanete = new ArrayList<Exoplanete>();
 		this.vueExoplanetes = vue;
+		daoExoplanetes = new DaoExoplanetes();
+		
+		listeExoplanete = daoExoplanetes.RecupererExoplanetes();
+		for (Exoplanete exoplanete : listeExoplanete) {
+			System.out.println(exoplanete);
+		}
 	}
 
 }
--- ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/dao/DaoExoplanetes.java
+++ ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/dao/DaoExoplanetes.java
@@ -1,7 +1,13 @@
 package ca.qc.cgmatane.informatique.exoplanete.dao;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 import ca.qc.cgmatane.informatique.exoplanete.modele.Exoplanete;
 
@@ -16,10 +22,33 @@ public class DaoExoplanetes
 	public List<Exoplanete> RecupererExoplanetes()
 	{
 		
+		Configuration configuration = new Configuration();
+		configuration.addClass(Exoplanete.class);
+		SessionFactory sessionControleur = configuration.buildSessionFactory();
+		Session session = sessionControleur.openSession();
 		
+		Transaction operation = null;
+		try
+		{
+			operation = session.beginTransaction();
+			
+		}
+		catch (Exception e) {
+			if(operation != null )  operation.rollback();
+			
+		}
+				
+		@SuppressWarnings("deprecation")
+		Iterator visiteurExoplanete = session.createQuery("from Exoplanete").iterate();
 		
-		
-		
+		while(visiteurExoplanete.hasNext())
+		{
+			Exoplanete exoplanete = (Exoplanete) visiteurExoplanete.next();
+			listeExoplanete.add(exoplanete);
+		}
+		session.close();
+		sessionControleur.close();
 		return listeExoplanete;
+		
 	}
 }
