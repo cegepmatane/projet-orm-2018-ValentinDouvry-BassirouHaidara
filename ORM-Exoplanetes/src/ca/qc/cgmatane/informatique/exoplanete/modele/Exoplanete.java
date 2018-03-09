6fb0c4131ec01a174c507903e0d42c8eb2ac1c22
DouvryValentin <valentin.jabba@gmail.com>
2018-03-08 22:59:06 -0500

    Ajout constructeur Exoplanete + getter

--- ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/modele/Exoplanete.java
+++ ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/modele/Exoplanete.java
@@ -24,6 +24,98 @@ public class Exoplanete {
 	 private String status;
 	 private String decouverte;
 	 
+	 public Exoplanete(String planete, String etoile, String typeEtoile, String masse, String rayon, String flux,
+			String temperature, String periode, String distance, String zone, float ist, float sph, String hzd,
+			String hzc, String hza, String pClasse, String hclasse, float phi, String distance2, String status,
+			String decouverte) {
+
+		this.planete = planete;
+		this.etoile = etoile;
+		this.typeEtoile = typeEtoile;
+		this.masse = masse;
+		this.rayon = rayon;
+		this.flux = flux;
+		this.temperature = temperature;
+		this.periode = periode;
+		this.distance = distance;
+		this.zone = zone;
+		this.ist = ist;
+		this.sph = sph;
+		this.hzd = hzd;
+		this.hzc = hzc;
+		this.hza = hza;
+		this.pClasse = pClasse;
+		this.hclasse = hclasse;
+		this.phi = phi;
+		this.distance2 = distance2;
+		this.status = status;
+		this.decouverte = decouverte;
+	}
+	public String getPlanete() {
+		return planete;
+	}
+	public String getEtoile() {
+		return etoile;
+	}
+	public String getTypeEtoile() {
+		return typeEtoile;
+	}
+	public String getMasse() {
+		return masse;
+	}
+	public String getRayon() {
+		return rayon;
+	}
+	public String getFlux() {
+		return flux;
+	}
+	public String getTemperature() {
+		return temperature;
+	}
+	public String getPeriode() {
+		return periode;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public String getZone() {
+		return zone;
+	}
+	public float getIst() {
+		return ist;
+	}
+	public float getSph() {
+		return sph;
+	}
+	public String getHzd() {
+		return hzd;
+	}
+	public String getHzc() {
+		return hzc;
+	}
+	public String getHza() {
+		return hza;
+	}
+	public String getpClasse() {
+		return pClasse;
+	}
+	public String getHclasse() {
+		return hclasse;
+	}
+	public float getPhi() {
+		return phi;
+	}
+	public String getDistance2() {
+		return distance2;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public String getDecouverte() {
+		return decouverte;
+	}
+	 
+	 
 	 
 	 
 
