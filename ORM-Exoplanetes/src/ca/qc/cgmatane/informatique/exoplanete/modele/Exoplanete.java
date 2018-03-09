f11512dd54931afd121d8fb098755928f926c99d
DouvryValentin <valentin.jabba@gmail.com>
2018-03-08 22:54:00 -0500

    Creation Classe Exoplanete + ajout fichier proprietes hibernate

---
+++ ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/modele/Exoplanete.java
@@ -0,0 +1,30 @@
+package ca.qc.cgmatane.informatique.exoplanete.modele;
+
+public class Exoplanete {
+	
+	 private String planete;
+	 private String etoile;
+	 private String typeEtoile;
+	 private String masse;
+	 private String rayon;
+	 private String flux;
+	 private String temperature;
+	 private String periode;
+	 private String distance;
+	 private String zone;
+	 private float ist;
+	 private float sph;
+	 private String hzd;
+	 private String hzc;
+	 private String hza;
+	 private String pClasse;
+	 private String hclasse;
+	 private float phi;
+	 private String distance2;
+	 private String status;
+	 private String decouverte;
+	 
+	 
+	 
+
+}
