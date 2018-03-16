d4fc509e6992fe8e2887629862574be79472b0cc
Bassirou1 <haidara.bassirou@yahoo.com>
2018-03-16 16:26:20 -0400

    creation des Bouton

--- ORM-Exoplanetes-Admin/src/ca/qc/cgmatane/informatique/exoplaneteAdmin/vue/VueExoplanetes.java
+++ ORM-Exoplanetes-Admin/src/ca/qc/cgmatane/informatique/exoplaneteAdmin/vue/VueExoplanetes.java
@@ -3,10 +3,13 @@ package ca.qc.cgmatane.informatique.exoplaneteAdmin.vue;
 
 import java.util.List;
 
+import javax.swing.JButton;
+
 import ca.qc.cgmatane.informatique.exoplaneteAdmin.action.ControleurExoplanetes;
 import ca.qc.cgmatane.informatique.exoplaneteAdmin.modele.Exoplanete;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -17,13 +20,15 @@ public class VueExoplanetes extends Application
 	protected StackPane racine;
  	protected String stringTest;
 	protected Text texteExoplanetes;
-	
+	protected JButton ajouter ;
+	protected JButton modifier ;
+	protected JButton effacer ;
 	public void start(Stage scenePrincipale)
 	{
 		//stringTest = "Hello word";
-		
+
 		this.racine = new StackPane();
-		
+
 		texteExoplanetes = new Text();
 		texteExoplanetes.setText(stringTest);
 		racine.getChildren().add(texteExoplanetes);
@@ -33,9 +38,17 @@ public class VueExoplanetes extends Application
 		scenePrincipale.setResizable(false);
 		scenePrincipale.show();
 		this.controleur = new ControleurExoplanetes(this);
-		
+		ajouter = new JButton();
+		modifier = new JButton();
+		effacer = new JButton();
+		ajouter.setText("Ajouter");
+		modifier.setText("Modifier");
+		effacer.setText("Effacer");
+
+
+
 	}
-	
+
 	public void afficherListeExoplanete(List<Exoplanete> listExoplanete)
 	{
 		String texte = "";
@@ -45,6 +58,6 @@ public class VueExoplanetes extends Application
 		}
 		this.texteExoplanetes.setWrappingWidth(500);
 		this.texteExoplanetes.setText(texte);
-		
+
 	}
 }
