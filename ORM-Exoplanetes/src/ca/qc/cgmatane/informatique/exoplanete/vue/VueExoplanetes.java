0eb71af391a082237dbd5cf430933adc37340168
Bassirou1 <haidara.bassirou@yahoo.com>
2018-03-26 21:16:20 -0400

    Creation des onglets

--- ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/vue/VueExoplanetes.java
+++ ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/vue/VueExoplanetes.java
@@ -7,6 +7,9 @@ import ca.qc.cgmatane.informatique.exoplanete.action.ControleurExoplanetes;
 import ca.qc.cgmatane.informatique.exoplanete.modele.Exoplanete;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -17,13 +20,28 @@ public class VueExoplanetes extends Application
 	protected StackPane racine;
  	protected String stringTest;
 	protected Text texteExoplanetes;
-	
+	protected Tab ongletListeExoplanete ;
+	protected Tab ongletMarquePage ;
+	protected Tab ongletRecherche;
+	protected Text texteListeExoplanete;
+	protected Text textMarquePage;
+	protected Text textRecherche;
+	protected TabPane ongleTabPane;
+
 	public void start(Stage scenePrincipale)
 	{
 		//stringTest = "Hello word";
-		
+
 		this.racine = new StackPane();
-		
+		 ongleTabPane = new TabPane();
+		 ongletListeExoplanete = new Tab("ListeExoplanete");
+		 ongletMarquePage = new Tab("MarquePage");
+		 ongletRecherche = new Tab("Recherche");
+		 ongleTabPane.getTabs().add(ongletListeExoplanete);
+		 ongleTabPane.getTabs().add(ongletMarquePage);
+		 ongleTabPane.getTabs().add(ongletRecherche);
+		 racine.getChildren().add(ongleTabPane);
+		 
 		texteExoplanetes = new Text();
 		texteExoplanetes.setText(stringTest);
 		racine.getChildren().add(texteExoplanetes);
@@ -33,9 +51,9 @@ public class VueExoplanetes extends Application
 		scenePrincipale.setResizable(false);
 		scenePrincipale.show();
 		this.controleur = new ControleurExoplanetes(this);
-		
+
 	}
-	
+
 	public void afficherListeExoplanete(List<Exoplanete> listExoplanete)
 	{
 		String texte = "";
@@ -45,6 +63,6 @@ public class VueExoplanetes extends Application
 		}
 		this.texteExoplanetes.setWrappingWidth(500);
 		this.texteExoplanetes.setText(texte);
-		
+
 	}
 }
