d39612a26504fd565cc9e10c4bab4589e65460f7
Douvry <valentin.jabba@gmail.com>
2018-03-27 01:10:57 -0400

    changement affichage liste exoplanetes onglet liste

--- ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/vue/VueExoplanetes.java
+++ ORM-Exoplanetes/src/ca/qc/cgmatane/informatique/exoplanete/vue/VueExoplanetes.java
@@ -6,18 +6,24 @@ import java.util.List;
 import ca.qc.cgmatane.informatique.exoplanete.action.ControleurExoplanetes;
 import ca.qc.cgmatane.informatique.exoplanete.modele.Exoplanete;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class VueExoplanetes extends Application
 {
 	protected ControleurExoplanetes controleur;
-	protected StackPane racine;
+	protected BorderPane racine;
  	protected String stringTest;
 	protected Text texteExoplanetes;
 	protected Tab ongletListeExoplanete ;
@@ -27,12 +33,13 @@ public class VueExoplanetes extends Application
 	protected Text textMarquePage;
 	protected Text textRecherche;
 	protected TabPane ongleTabPane;
+	protected ScrollPane scrollPane;
 
 	public void start(Stage scenePrincipale)
 	{
 		//stringTest = "Hello word";
 
-		this.racine = new StackPane();
+		this.racine = new BorderPane();
 		ongleTabPane = new TabPane();
 		ongletListeExoplanete = new Tab("ListeExoplanete");
 		ongletMarquePage = new Tab("MarquePage");
@@ -40,11 +47,12 @@ public class VueExoplanetes extends Application
 		ongleTabPane.getTabs().add(ongletListeExoplanete);
 		ongleTabPane.getTabs().add(ongletMarquePage);
 		ongleTabPane.getTabs().add(ongletRecherche);
-		racine.getChildren().add(ongleTabPane);
+		racine.setTop(ongleTabPane);
+		
+		this.scrollPane = new ScrollPane();
+
+		this.racine.setCenter(scrollPane);
 		 
-		texteExoplanetes = new Text();
-		texteExoplanetes.setText(stringTest);
-		racine.getChildren().add(texteExoplanetes);
 		Scene scene = new Scene(racine, 500,500);
 		scenePrincipale.setTitle("Exoplanetes");
 		scenePrincipale.setScene(scene);
@@ -56,13 +64,17 @@ public class VueExoplanetes extends Application
 
 	public void afficherListeExoplanete(List<Exoplanete> listExoplanete)
 	{
-		String texte = "";
+		VBox box = new VBox();
 		for (Exoplanete exoplanete : listExoplanete) {
-			texte+=exoplanete.getPlanete();
-			texte+= " - ";
-		}
-		this.texteExoplanetes.setWrappingWidth(500);
-		this.texteExoplanetes.setText(texte);
+			HBox hBox = new HBox();
+			Text nomPlanete = new Text();
+			String texte = "";
+			texte = exoplanete.getPlanete();
+			nomPlanete.setText(texte);
+			hBox.getChildren().addAll(nomPlanete);
+			box.getChildren().add(hBox);
 
+		}
+		this.scrollPane.setContent(box);
 	}
 }
